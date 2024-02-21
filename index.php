3e12fffdfec65347e14e7f07e8917b3f4e3ea434
Shahsvat rajyaguru <rajyagurushashvat@gmail.com>
2024-02-21 15:03:02 +0530

    frontend update

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -33,7 +33,71 @@
         <input type="submit" value="SUBMIT" name="submit">
     </form>
 </body>
+</html> -->
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		content="width=device-width, initial-scale=1.0">
+	<title>CHARUSAT</title>
+	<link rel="stylesheet"	href="style.css">
+        
+</head>
+
+<body>
+<header>
+    <a href="#"><img src="img.png" alt="Site Name" class="logo"/></a>
+    <h1 class="title">Campus Anti-Drug Logo Compition</h1>
+	<h2 class="title1">By: Anti-Drug and Alcohol Cell</h2>
+	<h2> </h2>
+	<h2 class="title1">Participants Details</h2>
+</header>
+		<!-- Form section that contains the
+			login and the signup form -->
+		<div class="form-section">
+			<!-- signup form -->
+            <form action="" method="post" enctype="multipart/form-data">
+                    <div class="signup-box">
+
+                        <input type="text"class="name ele" name="name" placeholder="Enter your name">
+                            <input type="text"
+                            class="name ele" name="id"
+                            placeholder="Enter your ID">
+
+                        <input type="email" name="email"
+                            class="email ele"
+                            placeholder="youremail@charusat.edu.in">
+
+                        <input type="integer" name="Number"
+                            class="password ele"
+                            placeholder="Number"  maxlength="10" onKeyPress="if(this.value.length==10) return false" >
+
+                            <select name="institute" class="email ele"> 
+                                <option value="DEPSTAR">DEPSTAR</option> 
+                                <option value="CSPIT">CSPIT</option> 
+                                <option value="PDPIAS">PDPIAS</option> 
+                                <option value="RPCP">RPCP</option> 
+                                <option value="CMPICA">CMPICA</option>
+                                <option value="AIRP">AIRP</option>  
+                                <option value="CIPS">CIPS</option> 
+                                <option value="MTIN">MTIN</option> 
+                                <option value="IIIM">IIIM</option> 
+                            </select>
+
+                        <input type="file" id="myFile" name="logo">
+
+                        <input type="submit" value="SUBMIT" class="clkbtn" name="submit">
+			        </div>
+            </form>
+				
+		</div>
+	</div>
+	<!-- <script src="sign.js"></script> -->
+</body>
 </html>
+
 
 <?php
     include './helperclasses.php';
@@ -52,16 +116,16 @@
 
         $obfile= new file_mani();
 
-        if(file_exists("files/"))
+        if(file_exists("Logos/"))
         {
-            if(file_exists("files/" . $id . ".jpg") || file_exists("files/" . $id . ".png") || file_exists("files/" . $id . ".jpeg")|| file_exists("files/" . $id . ".pdf"))
+            if(file_exists("Logos/" . $id . ".jpg") || file_exists("Logos/" . $id . ".png") || file_exists("Logos/" . $id . ".jpeg")|| file_exists("Logos/" . $id . ".pdf"))
             {
                 echo "<script>alert('There alrady an entry with this id.')</script>";
             }
             else
             {
                 try{
-                    $obfile->uplode("logo",$aloud_ext,"files/",$id);
+                    $obfile->uplode("logo",$aloud_ext,"Logos/",$id);
                     $obfile->insertIntoDb($id,$name,$email,$number,$institute,$timest,$con);
                 }
                 catch (Exception $e){
@@ -70,9 +134,9 @@
             }
         }
         else{
-            mkdir("files/");
+            mkdir("Logos/");
             try{
-                $obfile->uplode("logo",$aloud_ext,"files/",$id);
+                $obfile->uplode("logo",$aloud_ext,"Logos/",$id);
                 $obfile->insertIntoDb($id,$name,$email,$number,$institute,$timest,$con);
 
             }
---
+++ records.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 21, 2024 at 09:31 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `logo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `records`
+--
+
+DROP TABLE IF EXISTS `records`;
+CREATE TABLE IF NOT EXISTS `records` (
+  `id` varchar(10) NOT NULL COMMENT 'it will store id',
+  `name` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `contectNumber` int NOT NULL,
+  `institute` varchar(10) NOT NULL,
+  `timeStamp` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `records`
+--
+
+INSERT INTO `records` (`id`, `name`, `email`, `contectNumber`, `institute`, `timeStamp`) VALUES
+('d23dce158', 'Shahsvat rajyaguru', 'rajyagurushashvat@gmail.com', 2147483647, 'DEPSTAR', '2024-02-21'),
+('d23dce159', 'Shahsvat rajyaguru', 'rajyagurushashvat@gmail.com', 2147483647, 'DEPSTAR', '2024-02-21'),
+('d23dce174', 'Shahsvat rajyaguru', 'rajyagurushashvat@gmail.com', 2147483647, 'DEPSTAR', '2024-02-21'),
+('d23dce174', 'Shahsvat rajyaguru', 'rajyagurushashvat@gmail.com', 2147483647, 'DEPSTAR', '2024-02-21'),
+('d23dce112', 'shashvat', 'rajyagurushashvat@gmail.com', 0, 'DEPSTAR', '2024-02-21'),
+('d23dce112', 'shashvat', 'rajyagurushashvat@gmail.com', 0, 'DEPSTAR', '2024-02-21'),
+('d23dce112', 'shasvat', 'rajyagurushashvat@gmail.com', 0, 'DEPSTAR', '2024-02-21');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
